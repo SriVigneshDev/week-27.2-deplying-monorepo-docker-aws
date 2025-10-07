2e3a8bb62f1ad16a019292a39e482a24ff5954b4
Sri Vignesh <srivignesh.p@enfec.com>
2025-10-07 10:29:38 +0530

    feat: made fix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,15 +1,12 @@
-import { join } from 'path';
+import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = join(__filename, '..');
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: join(__dirname, '../../'),
-  },
+  outputFileTracingRoot: join(__dirname, '../../'),  
 };
 
 export default nextConfig;
