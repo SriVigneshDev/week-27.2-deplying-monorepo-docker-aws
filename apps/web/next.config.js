cb6a1ad246f41a5dc4401991acfb2c0bc845e6bb
Sri Vignesh <srivignesh.p@enfec.com>
2025-10-07 10:26:27 +0530

    feat: new dockerfile

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,15 @@
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, '..');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: join(__dirname, '../../'),
+  },
+};
 
 export default nextConfig;
